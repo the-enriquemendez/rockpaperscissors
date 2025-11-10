d33ce2f40f09771510dd56f27e2c4772499e8f1b
the-enriquemendez <128250445+the-enriquemendez@users.noreply.github.com>
2025-11-10 18:49:07 +0000

    Commit initial code

--- script.js
+++ script.js
@@ -1,7 +1,58 @@
-function getComputerChoice(){
-    const choices = ["rock", "paper", "scissors", "hammer"];
-    const random = Math.floor(Math.random() * choices.length); 
-    return choices[random];
+// two variables for the human and computer score
+
+function playGame(){;
+    let humanScore = 0;
+    let computerScore = 0;
+
+         function getComputerChoice(){
+            const choices = ["rock", "paper", "scissors"];
+            const random = Math.floor(Math.random() * choices.length); 
+            return choices[random];
+         }
+
+    function playRound(humanChoice, computerChoice){
+    humanChoice = humanChoice.toLowerCase(); // This translates the users input to lowercase, even if they use capitals.
+
+    if (humanChoice === computerChoice) {
+        console.log(`Its a tie!, human score, equals computer score`);
+     } else if(
+        (humanChoice === "rock" && computerChoice === "scissors") ||
+        (humanChoice === "paper" && computerChoice === "rock") ||
+        (humanChoice === "scissors" && computerChoice === "paper")
+    ){
+    console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+    humanScore++;
+    } else {
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+        computerScore++;
+    }
+
+    console.log(`Scores > human: ${humanScore}, computer: ${computerScore}`);  
+    }
+
+// Play 5 Rounds code
+for (let i = 1; i <= 5; i++) {
+    const humanChoice = getHumanChoice();
+    const computerChoice = getComputerChoice();
+    console.log(`Round ${i}:`);
+    playRound( humanChoice, computerChoice);
+    } 
+
+    // If statements to see whose the winner
+if (humanScore > computerScore){
+    console.log("Yay, you're the champion!");
+} else if (computerScore > humanScore){
+    console.log("The computer has won the game!");
+} else {
+    console.log("It's a tie, replay, the game to become champion!");
 }
 
-console.log(getComputerChoice());
+
+}
+function getHumanChoice(){
+    const choices = prompt("Enter Rock, Paper, or Scissors");
+    return choices
+}
+
+playGame();
+
